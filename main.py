2d71697d003d7afb7262ef9b1ff2c3d49344b680
shikamie <shikamireed@gmail.com>
2024-09-28 22:52:25 +0300

    3rd

---
+++ main.py
@@ -0,0 +1,17 @@
+# 1st program
+print((9**0.5)*5)
+print('')
+# 2nd program
+
+print(9.99 > 9.98 and 1000 != 1000.1)
+print('')
+# 3rd program
+
+
+print(2+2*2)
+print(2*(2+2))
+print((2+2*2) == (2*(2+2)))
+print('')
+
+# 4th program
+
