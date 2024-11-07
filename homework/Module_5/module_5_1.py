2a6a44b2fab6bfc74c456710b4a2a39c80b3db08
shikamie <shikamireed@gmail.com>
2024-11-07 21:04:39 +0300

    module_4_2.py

---
+++ homework/Module_5/module_5_1.py
@@ -0,0 +1,17 @@
+class House:
+	def __init__(self, name, number_of_flors):
+		self.name = name
+		self.number_of_flors = number_of_flors
+	def go_to(self, new_floor):
+		if 1 > new_floor or new_floor > self.number_of_flors:
+			print(f"Такого этажа не существует")
+		else:
+			print(f"Гость перешел на этаж {new_floor}")
+
+h1 = House('ЖК Горский', 18)
+
+h2 = House('Домик в деревне', 2)
+
+h1.go_to(0)
+
+h2.go_to(2)
