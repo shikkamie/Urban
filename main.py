1a58bb10c0745c362a4858f5a1e309f5f45ca847
Shikkami <shikamie@yandex.ru>
2024-12-14 20:59:42 +0300

    module_9_2

--- main.py
+++ main.py
@@ -1,30 +1,7 @@
-# 1st program
-print((9**0.5)*5)
-print('')
-# 2nd program
-
-print(9.99 > 9.98 and 1000 != 1000.1)
-print('')
-# 3rd program
+my_num = [3, 1, 5, 5, 4]
 
 
-print(2+2*2)
-print(2*(2+2))
-print((2+2*2) == (2*(2+2)))
-print('')
-
-# 4th program
-print("Plan a: ")
-a = '123.456'
-fl = float(a)
-i = int(fl * 10)
-print(i % 10)
-
-print("Plan B: ")
-b = '123.456'
-print(a.find('4')) # kek
-
-print("Plan C: ")
-c = '123.456'
-print(c[4]) # Строка - последовательность символов, то есть можно вывести порядковый номер нашего числа. Начинаем отсчёт с [0]
-
+def is_odd(num):
+    return num % 2
+result = filter(is_odd, my_num)
+print(list(result))
